6ac17158796d6feff6a59cc1dc5490509a61edfa
Bezoyan <zoya1905bezoyan@gmail.com>
2022-02-25 00:03:11 +0400

    Setup the logger

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const logger = require('morgan');
 
 const config = require('./configs/index');
 const router = require('./routes');
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -14,6 +18,36 @@ app.use(bodyParser.json())
 
 app.use(router);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  // error handlers
+  
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error: err
+      });
+    });
+  }
+  
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: {}
+    });
+  });
 
 app.listen(config.port, () => {
     console.log(`Running on PORT ${config.port}`)
